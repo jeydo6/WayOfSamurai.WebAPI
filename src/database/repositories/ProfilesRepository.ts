bc529dae44f5a91a609d3e501711afbd5e60c0a9
jeydo6 <Deryagin.Valdemar@yandex.ru>
2021-03-01 16:16:32 +0300

    profiles done;

--- src/database/repositories/ProfilesRepository.ts
+++ src/database/repositories/ProfilesRepository.ts
@@ -1,19 +1,20 @@
 import { Types } from 'mongoose';
 
+import IUser from '../models/User';
 import IProfile, { ProfileModel } from '../models/Profile';
 
 export default class ProfilesRepository {
     public static async findById(id: Types.ObjectId): Promise<IProfile | null> {
         return await ProfileModel.findById(id)
-            .populate('user')
-            .select('+name +status +photo')
+            .select('+name +status +photo -posts')
             .lean<IProfile>();
     }
 
     public static async findByUserId(userId: Types.ObjectId): Promise<IProfile | null> {
-        return await ProfileModel.findOne({ 'user._id': userId })
-            .populate('user')
-            .select('+name +status +photo')
+        const user = { _id: userId } as IUser;
+
+        return await ProfileModel.findOne({ user: user })
+            .select('+name +status +photo -posts')
             .lean<IProfile>();
     }
 
